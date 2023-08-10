f36bdf89d8d4217a013ba5538284ddfda3440c58
Felipe Chiquito <felipechiquito010@gmail.com>
2023-08-10 00:32:58 -0500

    second commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+    exportPathMap: async function (
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+    ) {
+        return {
+            '/': { page: '/' },
+            '/about': { page: '/about' },
+        }
+    },
     reactStrictMode: true,
     optimizeFonts: true,
     images: {
